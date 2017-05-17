3e954c56dae92fd941f2415af123d5282a594d9c
Leonid Zlotnikov <zlotnleo@gmail.com>
2017-05-17 14:11:01 +0100

    Examples of image background and event handling

--- src/uk/ac/cam/intdes/gr1/App.java
+++ src/uk/ac/cam/intdes/gr1/App.java
@@ -1,10 +1,11 @@
 package uk.ac.cam.intdes.gr1;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -12,16 +13,23 @@ import uk.ac.cam.intdes.gr1.ui.MainFrame;
 import uk.ac.cam.intdes.gr1.ui.TopPanel;
 import uk.ac.cam.intdes.gr1.ui.SettingsContent;
 
+
 public class App extends Application {
     private Scene scene;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //Example
+        //Example: Should be split into appropriate classes
         Pane left_contol = new Pane();
         left_contol.setBackground(new Background(new BackgroundFill(Color.AQUA, null, null)));
         Pane right_control = new Pane();
-        right_control.setBackground(new Background(new BackgroundFill(Color.BEIGE, null, null)));
+        right_control.setBackground(new Background(new BackgroundImage(new Image("/gear.png"),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                new BackgroundSize(
+                        1.0, 1.0, true, true,
+                        true, false
+                ))));
+        right_control.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> System.exit(1));
         SettingsContent sc = new SettingsContent(/*params*/);
 
         scene = MainFrame.createScene(new TopPanel(left_contol, "WTS", right_control), sc);
--- src/uk/ac/cam/intdes/gr1/ui/IAppScreen.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.cam.intdes.gr1.ui;
-
-import javafx.scene.Scene;
-
-public interface IAppScreen {
-    Scene getLeftButtonScreen();
-    Scene getRightButtonScreen();
-}
