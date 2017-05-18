46378c6438d28de1717b3b8a5c249e34b4d5b18b
Minstrels <jack.deeley@ntlworld.com>
2017-05-18 13:56:03 +0100

    Basic beginnings of SettingPane

--- src/uk/ac/cam/intdes/gr1/ui/SettingPane.java
+++ src/uk/ac/cam/intdes/gr1/ui/SettingPane.java
@@ -1,10 +1,19 @@
 package uk.ac.cam.intdes.gr1.ui;
-import uk.ac.cam.intdes.gr1.*;
 
-public class SettingPane {
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+
+public class SettingPane extends GridPane{
 
     public SettingPane(){
-
+        ColumnConstraints c1 = new ColumnConstraints(), c2 = new ColumnConstraints();
+        c1.setPercentWidth(50); c2.setPercentWidth(50);
+        // c1 contains setting names, c2 contains setting values
+        Label lb1 = new Label("Temperature Units:");
+        Label lb2 = new Label("Distance Units:");
+        setConstraints(lb1,0,0);
+        setConstraints(lb2, 0,2);
+        // Need to clear up exactly how formatting is to be done.
     }
 
 }
