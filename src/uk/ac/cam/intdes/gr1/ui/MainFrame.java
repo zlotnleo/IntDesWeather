e0c67069c5c52d053bd63007246613232e45cf20
Vlad Badelita <vladbadelita@gmail.com>
2017-05-18 14:24:50 +0100

    Added leo's branch manually

---
+++ src/uk/ac/cam/intdes/gr1/ui/MainFrame.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.intdes.gr1.ui;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+public class MainFrame {
+    public static Scene createScene(TopPanel tp, Content contents){
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(tp, contents);
+        return new Scene(vbox);
+    }
+}
