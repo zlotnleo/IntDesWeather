1c52588ad90baf47a1717d52c929e33a4b3df5ec
Minstrels <jack.deeley@ntlworld.com>
2017-05-19 20:58:21 +0100

    Settings now load from text file settings.txt

--- src/uk/ac/cam/intdes/gr1/ui/SettingsContent.java
+++ src/uk/ac/cam/intdes/gr1/ui/SettingsContent.java
@@ -1,13 +1,28 @@
 package uk.ac.cam.intdes.gr1.ui;
+import java.io.*;
 
 public class SettingsContent extends Content {
+
+	// TODO: save settings on app close.
 
     private static SettingsContent instance = new SettingsContent();
 
     private SettingsContent(){
         super();
+		boolean tempMode = false, distMode = false;
+        try{
+			BufferedReader bfr = new BufferedReader(new FileReader("settings.txt"));
+			tempMode = Boolean.parseBoolean(bfr.readLine());
+			distMode = Boolean.parseBoolean(bfr.readLine());
+		}
+		catch(FileNotFoundException fnfe){
+        	System.out.println("Failed to find file settings.txt - check file path?");
+		}
+		catch(IOException ioe){
+			System.out.println("ReadLine failed.");
+		}
 
-        SettingPane SPane = new SettingPane((int)this.getPrefHeight(), (int)this.getPrefWidth());
+        SettingPane SPane = new SettingPane((int)this.getPrefHeight(), (int)this.getPrefWidth(), tempMode, distMode);
         this.getChildren().add(SPane);
     }
 
